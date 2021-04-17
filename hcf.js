6046a04f81c83803c089b7ec1c9c847eb9c89147
Praneet Dixit <65533196+PraneetDixit@users.noreply.github.com>
2021-04-17 14:00:50 +0530

    Update hcf.js

--- hcf.js
+++ hcf.js
@@ -1,5 +1,20 @@
 // Finding HCF of two numbers using Euclid's division algorithm
 
-function hcf (num1, num2){
-  return result;
+function hcf(n1, n2){
+	let a = Math.max(n1, n2);
+	let b = Math.min(n1, n2);
+	let r = a%b;
+	
+	while(r){
+		a = b;
+		b = r;
+		r = a%b;
+	}
+	
+	return b;
 }
+
+hcf(5, 5); // 5
+hcf(196, 38220); // 196
+hcf(867, 255); // 51
+hcf(4052, 12567); // 4
