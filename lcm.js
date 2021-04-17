da9dfd5002dab49fa3550854680ebcbd0acf557a
Praneet Dixit <65533196+PraneetDixit@users.noreply.github.com>
2021-04-17 23:22:02 +0530

    Update lcm.js

--- lcm.js
+++ lcm.js
@@ -1,6 +1,8 @@
 // Finding lcm of two integers "a" and "b" by property :-
 //HCF(a, b) * LCM(a, b) = a*b
 
+import hcf from './hcf'; // Helper function
+
 function lcm(a, b){
-  return result;  
+  return (a*b)/hcf(a, b);  
 }
