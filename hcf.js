a27c24b9621f0c7ecbffd33927c6b9dc166642fe
Praneet Dixit <65533196+PraneetDixit@users.noreply.github.com>
2021-04-30 15:10:31 +0530

    update: add implementation for more than two numbers

--- hcf.js
+++ hcf.js
@@ -1,9 +1,14 @@
-// Finding HCF of two numbers using Euclid's division algorithm
+// Finding HCF of numbers using Euclid's division algorithm
 
-function hcf(n1, n2){
+/**
+** Core function :-
+** Accepts two integers num1 and num2
+**/
+
+function hcf(num1, num2){
 	let a = Math.max(n1, n2);
 	let b = Math.min(n1, n2);
-	let r = 1;
+	let r = a;
 	while(r){
 		a = b;
 		b = r;
@@ -12,4 +17,15 @@ function hcf(n1, n2){
 	return b;
 }
 
-export {hcf as default}; // For importing in other files
+/**
+** Implementation for more than two numbers :-
+** Accepts and array arr of integers
+**/
+
+function hcfMultiple(arr){
+	let init = hcf(arr[0], arr[1]);
+	for(let i=2; i<arr.length; i++){
+		init = hcf(init, arr[i]);
+	}
+	return init;
+}
